9841f0b6f98127d53b62ec1ff1ad7c89a19b052f
scotth527 <scottyh527@gmail.com>
2021-02-09 10:28:08 -0500

    Added namespace to both profiles, and posts, added to posts details page, added test for posts index

--- microblog/settings.py
+++ microblog/settings.py
@@ -110,7 +110,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
+DATE_FORMAT = "Y-m-d"
 USE_L10N = True
 
 USE_TZ = True
--- profiles/admin.py
+++ profiles/admin.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 # Register your models here.
 from profiles.models import Profile
 
+app_name="profiles"
 admin.site.register(Profile)
