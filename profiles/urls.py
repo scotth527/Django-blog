892352564e57909898a72c913983a5a08f4cb692
scotth527 <scottyh527@gmail.com>
2021-02-10 21:38:49 -0500

    login function works, need to fix bug with registration not inputting the profile information

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 app_name = 'profiles'
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^signin/$', views.signin, name='signup'),
+    url( r'^login/$', views.signin, name="login"),
+
 ]
