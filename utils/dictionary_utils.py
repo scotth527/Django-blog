c7a40c0c2d4764c244796579306f47490803cbd0
scotth527 <scottyh527@gmail.com>
2021-02-28 23:45:19 -0500

    like button is not updating in detail view, need to investigate

--- utils/dictionary_utils.py
+++ utils/dictionary_utils.py
@@ -1,9 +1,11 @@
-# Given a list of dictionaries check if list contains a dictory with the key/value
 def check_existing_dictionary_in_list( list , key, value):
+    '''
+    Given a list of dictionaries check if list contains a dictory with the key/value
+    '''
     it_contains_value = False
 
     for item in list:
-        if item[key] == value:
+        if getattr(item, key , False) == value:
             it_contains_value = True
             break
 
