cd74822f599a88e4a525bbd9f39f9267719b8580
scotth527 <scottyh527@gmail.com>
2021-03-12 19:12:20 -0500

    added friendship table to admin, pending friendship requets are show in users profile page

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,7 +2,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from profiles.models import Profile
+from profiles.models import Profile, Friendship
 
 app_name="profiles"
 admin.site.register(Profile)
+admin.site.register(Friendship)
