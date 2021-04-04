bd00370a4f1f2dc6060ba282a08de61eb138c7da
scottyh527 <scottyh527@gmail.com>
2021-04-04 09:28:23 -0400

    refactored profiles urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -11,11 +11,14 @@ urlpatterns = [
     url( r'^login/$', views.signin, name="login"),
     url( r'^logout/$', views.logout_view, name="logout"),
     url(r'search/$', login_required(basic_search), name='basic_search'),
-    path('<int:pk>', views.DetailView.as_view(), name='detail'),
     # path('<str:keyword>/search', views.SearchUserIndexView.as_view(), name='user-search'),
     path('<int:requestee_id>/friend-request', views.request_friendship, name='friend-request'),
-    path('<int:pk>/friend-request-update', views.FriendshipUpdateView.as_view(), name='friend-request-update'),
-    path('<int:pk>/friend-list', views.FriendshipIndexView.as_view(), name='friend-list'),
-    path('<int:pk>/<int:suggestion_count>/friend-suggestion', views.FriendshipSuggestionIndexView.as_view(), name='friend-suggestion'),
-    path('<int:pk>/friendship-delete', views.FriendshipDeleteView.as_view(), name='delete-friendship'),
+    path('<int:pk>/', include([
+        path('', views.DetailView.as_view(), name='detail'),
+        path('friend-request-update', views.FriendshipUpdateView.as_view(), name='friend-request-update'),
+        path('friend-list', views.FriendshipIndexView.as_view(), name='friend-list'),
+        path('<int:suggestion_count>/friend-suggestion', views.FriendshipSuggestionIndexView.as_view(),
+             name='friend-suggestion'),
+        path('friendship-delete', views.FriendshipDeleteView.as_view(), name='delete-friendship'),
+    ]))
 ]
