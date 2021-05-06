f70da6d745bc85e5e58646119ff163794a4205cd
scottyh527 <scottyh527@gmail.com>
2021-05-06 19:31:30 -0400

    working on autocomplete ui

--- microblog/static/microblog/js/main.js
+++ microblog/static/microblog/js/main.js
@@ -1,3 +1,5 @@
+let timeout;
+
 let fade_out = ()=> {
     $(".messages").fadeOut().empty();
 }
@@ -8,9 +10,15 @@ let resetMessages = setTimeout(fade_out, 3000);
 //Search autocomplete
 
 let searchBox = document.getElementById('id_q');
+let autocompleteContainer = document.getElementById('autocomplete-suggestions')
 
 searchBox.onchange = (e)=> {
     console.log("E", e.target.value)
+    if (!e.target.value) {
+        autocompleteContainer.classList.add('d-none')
+    } else {
+        autocompleteContainer.classList.remove('d-none')
+    }
     clearTimeout(timeout);
     timeout = setTimeout(() => onSearch(e.target.value), 800);
 }
