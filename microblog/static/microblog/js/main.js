ec1b411d73e47906527fe248561a0fddfaf106b5
scottyh527 <scottyh527@gmail.com>
2021-05-05 20:17:28 -0400

    working on autocomplete

--- microblog/static/microblog/js/main.js
+++ microblog/static/microblog/js/main.js
@@ -5,3 +5,16 @@ let fade_out = ()=> {
 //Clear messages after 3 seconds
 let resetMessages = setTimeout(fade_out, 3000);
 
+//Search autocomplete
+
+let searchBox = document.getElementById('id_q');
+
+searchBox.onchange = (e)=> {
+    console.log("E", e.target.value)
+    clearTimeout(timeout);
+    timeout = setTimeout(() => onSearch(e.target.value), 800);
+}
+
+let onSearch = ()=> {
+
+}
